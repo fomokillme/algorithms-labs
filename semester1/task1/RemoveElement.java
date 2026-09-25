9854bdd5b8a6e3a41594ffd55737494375fadd27
fomokillme <fomokillme@gmail.com>
2026-09-25 13:56:41 +0600

    Create RemoveElement.java

---
+++ semester1/task1/RemoveElement.java
@@ -0,0 +1,15 @@
+public class RemoveElement {
+
+    public int removeElementInplace(int[] arr, int val) {
+        int left = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] != val) {
+                arr[left] = arr[right];
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
